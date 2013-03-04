5d1560db354d93ebfb3ddd61e8a256cd63112a3b
pstoll <perry@pstoll.com>
2013-03-04 03:41:53 -0800

    Initial posting

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 diy-lyse-pdf
 ============
 
-A stupid shell script to create a reasonable PDF of the Learn-You-Some-Erlang site
+Do It Yourself Learn-You Some Erlang Portable Document Format, AKA D.L.P.
+
+A simple shell script that downloads and creates a reasonable PDF of the Learn-You-Some-Erlang site, with clickable indexes and such.
+
+Requirements
+===
+
+You'll need htmldoc.
+
+On Mac OSX, it's a simple
+
+    brew install htmldoc
+
+Usage
+===
+
+Assuming you are on a unix-y OS (linux or MacOSX or a reasonable cygwin installation on Windows...), the following should do it:
+
+    ./gen-lyse-pdf.sh
+
+You'll have a PDF named LYSE-book.pdf in the current directory.
+
+If something goes wrong, run it like this to see what's going on.:
+
+    sh -x ./gen-lyse-pdf.sh
+
---
+++ gen-LYSE-pdf.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# Written by Perry Stoll
+#
+#  available on http://github.com/pstoll/diy-lyse-pdf
+#
+# This work is made available in the public domain.
+# 
+# simple script to generate a reasonable PDF of LYSE
+# downloads site into the current directory and generates a PDF.
+# requires htmldoc tool
+
+wget -k --mirror http://learnyousomeerlang.com/
+
+pushd learnyousomeeralang.com
+
+files=`cat contents | perl -ne 'm/class="local chapter"/ && m/href="([^"#]+)"/ && print "$1\n"'`
+
+# do a little cleanup on most pages:
+# remove headers and footer nav from most content pages
+#  but not the content file, it breaks book mode
+for file in $files faq
+do
+  echo $file; 
+   perl -0p -i.bak -e 's#<div id="wrapper">.*?</noscript></div>##gs; s#<ul class="navigation">.*?</ul>##gs;' $file
+done
+
+# create a nice PDF, with cross linking and all
+htmldoc --outfile ../LYSE-book.pdf --book contents $files faq
+
+popd
